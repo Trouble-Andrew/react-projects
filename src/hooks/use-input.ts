d97068c8d809be9ebf1764d369baf37566644f81
Andrey Terekhov <satyr663@gmail.com>
2022-12-15 17:30:49 +0600

    refactor: rewrite input hook logic with useReducer

--- src/hooks/use-input.ts
+++ src/hooks/use-input.ts
@@ -1,27 +1,68 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+interface InputState {
+  value: string;
+  isTouched: boolean;
+}
+
+type ACTIONTYPE =
+  | { type: 'INPUT'; value: string }
+  | { type: 'BLUR' }
+  | { type: 'RESET' };
+
+const initialInputState = {
+  value: '',
+  isTouched: false,
+};
+
+const inputStateReducer = (state: InputState, action: ACTIONTYPE) => {
+  if (action.type === 'INPUT') {
+    return {
+      value: action.value,
+      isTouched: state.isTouched,
+    };
+  }
+
+  if (action.type === 'BLUR') {
+    return {
+      value: state.value,
+      isTouched: true,
+    };
+  }
+
+  if (action.type === 'RESET') {
+    return {
+      value: '',
+      isTouched: false,
+    };
+  }
+
+  return initialInputState;
+};
 
 const useInput = (validateValue: (...args: any) => boolean) => {
-  const [enteredValue, setEnteredValue] = useState('');
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState,
+  );
 
-  const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
   const valueChangeHandler = (e: React.FormEvent<HTMLInputElement>) => {
-    setEnteredValue(e.currentTarget.value);
+    dispatch({ type: 'INPUT', value: e.currentTarget.value });
   };
 
   const inputBlurHandler = () => {
-    setIsTouched(true);
+    dispatch({ type: 'BLUR' });
   };
 
   const reset = () => {
-    setEnteredValue('');
-    setIsTouched(false);
+    dispatch({ type: 'RESET' });
   };
 
   return {
-    value: enteredValue,
+    value: inputState.value,
     isValid: valueIsValid,
     hasError,
     valueChangeHandler,
