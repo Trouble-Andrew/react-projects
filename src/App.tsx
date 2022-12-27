37341e6c916a9f9f79985da36811a51d9888117e
Andrey Terekhov <satyr663@gmail.com>
2022-12-27 13:44:54 +0600

    feat: deploy project to firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
+import React, { Suspense } from 'react';
 import Layout from 'components/layout/Layout';
-import AllQuotes from 'pages/AllQuotes';
-import NewQuote from 'pages/NewQuote';
-import NotFound from 'pages/NotFound';
-import QuoteDetail from 'pages/QuoteDetail';
-import React from 'react';
 import { Route, Navigate, Routes } from 'react-router-dom';
+import LoadingSpinner from 'components/UI/LoadingSpinner';
+
+const NewQuote = React.lazy(() => import('pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('pages/NotFound'));
+const AllQuotes = React.lazy(() => import('pages/AllQuotes'));
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate to="/quotes" />} />
-        <Route path="/quotes" element={<AllQuotes />} />
-        <Route path="/quotes/:quoteId/*" element={<QuoteDetail />} />
-        <Route path="/new-quote" element={<NewQuote />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Navigate to="/quotes" />} />
+          <Route path="/quotes" element={<AllQuotes />} />
+          <Route path="/quotes/:quoteId/*" element={<QuoteDetail />} />
+          <Route path="/new-quote" element={<NewQuote />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
